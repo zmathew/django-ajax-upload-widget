c69c014ca0b460e25b65fea6ced2b4b2c86267ea
Zach Mathew <zmathew.to@gmail.com>
2012-08-26 16:01:40 -0400

    fixed MANIFEST and changed version

--- ajax_upload/__init__.py
+++ ajax_upload/__init__.py
@@ -1,5 +1,5 @@
 """Provides AJAX file upload functionality for FileFields and ImageFields with a simple widget replacement in the form."""
 
-VERSION = (0, 5, 0)
+VERSION = (0, 5, 1)
 
 __version__ = '.'.join(map(str, VERSION))
