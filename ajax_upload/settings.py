2b8c20c42c927a3b6bfb446cc90d282253e10809
Zach Mathew <zmathew@freshbooks.com>
2013-06-17 04:51:18 -0400

    Removes unused setting

--- ajax_upload/settings.py
+++ ajax_upload/settings.py
@@ -1,6 +1,1 @@
 from django.conf import settings
-
-
-# Number of seconds to keep uploaded files. The clean_uploaded command will
-# delete them after this has expired.
-UPLOADER_DELETE_AFTER = getattr(settings, 'UPLOADER_DELETE_AFTER', 60 * 60)
