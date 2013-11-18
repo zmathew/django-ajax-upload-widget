e9c71696751a3fbefced61f82614cea23ec26820
Zach Mathew <zmathew.to@gmail.com>
2013-11-18 12:26:18 -0500

    Increments version to 0.5.5

--- ajax_upload/__init__.py
+++ ajax_upload/__init__.py
@@ -1,5 +1,5 @@
 """Provides AJAX file upload functionality for FileFields and ImageFields with a simple widget replacement in the form."""
 
-VERSION = (0, 5, 4)
+VERSION = (0, 5, 5)
 
 __version__ = '.'.join(map(str, VERSION))
