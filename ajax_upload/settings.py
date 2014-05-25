040d48059b5a18f945f1711d3c8713b1efb533ac
Felix Plitzko <felix.plitzko@libermentix.com>
2014-05-25 13:06:37 +0200

    Update settings.py

--- ajax_upload/settings.py
+++ ajax_upload/settings.py
@@ -2,4 +2,4 @@ from django.conf import settings
 
 
 FILE_FIELD_MAX_LENGTH = getattr(settings, 'AJAX_UPLOAD_FILE_FIELD_MAX_LENGTH', 255)
-UPLOAD_TO_DIRECTORY = getattr(settings, 'AJAX_UPLOAD_FILE_TARGET_DIRECTORY', 'ajax_upload/')
+UPLOAD_TO_DIRECTORY = getattr(settings, 'AJAX_UPLOAD_FILE_TARGET_DIRECTORY', 'ajax_uploads/')
