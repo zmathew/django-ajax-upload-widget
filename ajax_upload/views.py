37c5220734437b77de16f37fba6f6491d0b84b3c
Andrew Kornilov <frutik@gmail.com>
2014-08-15 13:08:12 +0300

    -

--- ajax_upload/views.py
+++ ajax_upload/views.py
@@ -5,11 +5,15 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
 from ajax_upload.forms import UploadedFileForm
-
+fromt ajax_upload.settings import UPLOAD_PERMISSION_CHECKER as permission_checker
 
 @csrf_exempt
 @require_POST
 def upload(request):
+    if callable(permission_checker):
+        errors = permission_checker(request)
+        if errors is not None:
+            return HttpResponseBadRequest(json.dumps(errors))
     form = UploadedFileForm(data=request.POST, files=request.FILES)
     if form.is_valid():
         uploaded_file = form.save()
